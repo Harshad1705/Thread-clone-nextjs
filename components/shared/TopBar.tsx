3fa1dc8029dc52f851d970cf368e8e81c6be668c
Harshad Lande <77875603+Harshad1705@users.noreply.github.com>
2023-12-30 16:00:22 +0530

    ignore ts errors

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,7 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { dark, neobrutalism } from "@clerk/themes";
+import { dark } from "@clerk/themes";
 
 const TopBar = () => {
   return (
